14f89827ef8582d0c549f6a4528d06e055086a37
Tyler Mills <tmills@monin.com>
2016-12-06 18:41:56 -0500

    Add diff, execute commands and tidy migrate command

---
+++ src/Console/Commands/Migrations/ExecuteCommand.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Etre\Shell\Console\Commands\Migrations;
+
+use Doctrine\DBAL\Migrations\Tools\Console\Command\ExecuteCommand as DoctrineExecuteCommand;
+use Etre\Shell\Helper\MigrationsHelper;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ExecuteCommand extends DoctrineExecuteCommand {
+
+    protected $migrationsHelper;
+
+    protected function configure() {
+
+        parent::configure();
+        $this->setName('etre:migrations:execute');
+        $this->migrationsHelper = new MigrationsHelper();
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->getApplication()->initMagento();
+        $this->migrationsHelper->initEtreConfig($input);
+
+        parent::execute($input,$output);
+    }
+
+}
