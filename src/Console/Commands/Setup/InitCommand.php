6ba30e4772a8a4b70b0025d72106e860aa81f1a5
Tyler Mills <tmills@monin.com>
2016-12-07 08:59:59 -0500

    Changes symlink functionality - relative paths are being used

--- src/Console/Commands/Setup/InitCommand.php
+++ src/Console/Commands/Setup/InitCommand.php
@@ -9,9 +9,11 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\ConfirmationQuestion;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Finder\Finder;
 
-class InitCommand extends Command
-{
+class InitCommand extends Command {
+    protected $fileSystem;
     /** @var DirectoryHelper $directoryHelper */
     protected $directoryHelper;
 
@@ -23,15 +25,13 @@ class InitCommand extends Command
      * PatchCommand constructor.
      * @param $patchHelper
      */
-    public function __construct($name = null)
-    {
+    public function __construct($name = NULL) {
         $this->directoryHelper = new DirectoryHelper();
-
+        $this->fileSystem = new Filesystem();
         parent::__construct($name);
     }
 
-    public function configure()
-    {
+    public function configure() {
         $this
             ->setName('etre:setup:init')
             ->addArgument("site-path", InputArgument::OPTIONAL, 'Defaults to "public"', "public")
@@ -39,16 +39,15 @@ class InitCommand extends Command
             ->setHelp("Create a Magento \"public\" director to assist in protecting application code.");
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output)
-    {
-
+    protected function execute(InputInterface $input, OutputInterface $output) {
+        $this->sanitizeSitePathArgument($input);
         $directoryHelper = $this->directoryHelper;
         $magentoDirectory = $directoryHelper->getApplicationDirectory();
         $this->publicDirName = $magentoDirectory . $directoryHelper::DS . $input->getArgument("site-path");
-        if(file_exists($this->publicDirName)):
+        if ($this->fileSystem->exists($this->publicDirName)):
             $helper = $this->getHelper('question');
-            $question = new ConfirmationQuestion("<info>{$this->publicDirName} directory already exists. Do you want to continue delete this directory and continue?</info><comment>[Default: No]</comment> ", false);
-            if(!$helper->ask($input, $output, $question)) {
+            $question = new ConfirmationQuestion("<info>{$this->publicDirName} directory already exists. Do you want to continue delete this directory and continue?</info><comment>[Default: No]</comment> ", FALSE);
+            if (!$helper->ask($input, $output, $question)) {
                 return;
             }
             $directoryHelper->delTree($this->publicDirName);
@@ -57,14 +56,15 @@ class InitCommand extends Command
         $output->writeln("<info>Created ./public directory</info>");
         $this->createIndex();
         $output->writeln("<info>Creating .gitignore.</info>");
-        $this->createGitIgnore();
+/*        $this->createGitIgnore();*/
         $this->cp(".htaccess", $output);
-        $this->createLink("js", $output);
-        $this->createLink("media", $output);
-        $this->createLink("skin", $output);
-        $this->createLink("favicon.ico", $output);
-        $this->createLink("sitemap", $output);
-        if($input->hasArgument("site-path")):
+        $this->createLink("js", $input, $output);
+        $this->createLink("media", $input, $output);
+        $this->createLink("skin", $input, $output);
+        $this->createLink("favicon.ico", $input, $output);
+        $this->createLink("sitemap", $input, $output);
+        $this->createLink("wp", $input, $output);
+        if ($input->hasArgument("site-path")):
             $completeMessage = "<comment>All done! A Magento site can now be pointed to </comment>{$input->getArgument('site-path')}";
         else:
             $completeMessage = "<comment>All done! You can set your document root to </comment>{$this->publicDirName}";
@@ -73,7 +73,7 @@ class InitCommand extends Command
             $completeMessage,
             "\t<info>1. Make sure your server can access </info>{$this->publicDirName}",
             "\t<info>2. This directory uses symbolic links. Magento's Apache rules enable this by default and should not cause a problem.</info>",
-            "\t<info>3. A .gitignore file has been created.</info>",
+/*            "\t<info>3. A .gitignore file has been created.</info>",*/
             "\t\t<info>- The contents in this directory will not be added to a Git repository since the symbolic links paths are absolute.</info>",
             "\t\t<info>- This command will need to be executed again in production if the file paths are different.</info>",
         ]);
@@ -83,13 +83,11 @@ class InitCommand extends Command
     /**
      * @return bool
      */
-    protected function makePublicDirectory()
-    {
-        return mkdir($this->publicDirName);
+    protected function makePublicDirectory() {
+        return $this->fileSystem->mkdir($this->publicDirName);
     }
 
-    protected function createIndex()
-    {
+    protected function createIndex() {
         $directoryHelper = $this->directoryHelper;
         $magentoDirectory = $directoryHelper->getApplicationDirectory();
         $pathToPublic = $this->publicDirName;
@@ -108,8 +106,7 @@ class InitCommand extends Command
         file_put_contents($indexFile, $fileContents);
     }
 
-    protected function createGitIgnore()
-    {
+    protected function createGitIgnore() {
         $directoryHelper = $this->directoryHelper;
         $pathToPublic = $this->publicDirName;
         $fileContents = '*';
@@ -120,18 +117,8 @@ class InitCommand extends Command
     /**
      * @param OutputInterface $output
      */
-    protected function createLink($magentoPath, OutputInterface $output)
-    {
-        $this->symlinkPublicMageRoot($magentoPath);
-        $output->writeln("<info>Linked $magentoPath in public directory</info>");
-    }
-
-    /**
-     * @param OutputInterface $output
-     */
-    protected function cp($magentoPath, OutputInterface $output)
-    {
-        $style = new OutputFormatterStyle('red', 'yellow', array('bold', 'blink'));
+    protected function cp($magentoPath, OutputInterface $output) {
+        $style = new OutputFormatterStyle('red', 'yellow', ['bold', 'blink']);
         $output->getFormatter()->setStyle('warning', $style);
 
         $directoryHelper = $this->directoryHelper;
@@ -139,7 +126,7 @@ class InitCommand extends Command
         $pathToPublic = $this->publicDirName;
         $destination = $pathToPublic . $directoryHelper::DS . $magentoPath;
         $source = $magentoDirectory . $directoryHelper::DS . $magentoPath;
-        copy($source,$destination);
+        copy($source, $destination);
         $output->writeln([
             "<info>Copied</info> $magentoPath <info>to </info> $destination",
             "<warning>Notice:</warning> <comment>If the path to the new site is </comment>http://[magento-root]/newsite...",
@@ -148,22 +135,54 @@ class InitCommand extends Command
         ]);
     }
 
-    protected function symlinkPublicMageRoot($imitationDirectoryName)
-    {
-        $directoryHelper = $this->directoryHelper;
-        $magentoDirectory = $directoryHelper->getApplicationDirectory();
-        $pathToPublic = $this->publicDirName;
-        $link = $pathToPublic . $directoryHelper::DS . $imitationDirectoryName;
-        $target = $magentoDirectory . $directoryHelper::DS . $imitationDirectoryName;
+    /**
+     * @param OutputInterface $output
+     */
+    protected function createLink($magentoFileOrDirName, InputInterface $input, OutputInterface $output) {
+        $relativePathToMagentoAsset = $this->getRelativePathToRoot($input) . $magentoFileOrDirName;
+        $absolutePathToMagentoAsset = $this->directoryHelper->getApplicationDirectory() . DIRECTORY_SEPARATOR . $magentoFileOrDirName;
+        $link = $magentoFileOrDirName;
 
-        return @symlink($target, $link);
+        if ($this->fileSystem->exists($absolutePathToMagentoAsset)):
+            chdir($this->publicDirName);
+            $this->fileSystem->symlink($relativePathToMagentoAsset, $link);
+            chdir($this->directoryHelper->getApplicationDirectory());
+            $output->writeln("<info>Linked $magentoFileOrDirName in public directory</info>");
+        else:
+            $output->writeln('<error>Could not link to ' . $magentoFileOrDirName . '. The file or directory does not exist in your Magento root.</error>');
+        endif;
+    }
+
+    /**
+     * @param $magentoPath
+     * @param \Symfony\Component\Console\Input\InputInterface $input
+     * @return string
+     */
+    protected function getRelativePathToRoot(InputInterface $input) {
+        $subDirLevels = array_filter(explode(DIRECTORY_SEPARATOR, $input->getArgument("site-path")));
+        $relativePath = "";
+        foreach ($subDirLevels as $level):
+            $relativePath .= "../";
+        endforeach;
+
+        return $relativePath;
     }
 
     /**
      * @param OutputInterface $output
      */
-    protected function writeBlankLn(OutputInterface $output)
-    {
+    protected function writeBlankLn(OutputInterface $output) {
         $output->writeln("");
+    }
+
+    /**
+     * @param \Symfony\Component\Console\Input\InputInterface $input
+     * @return mixed
+     */
+    protected function sanitizeSitePathArgument(InputInterface $input) {
+        $sitePathesToArray = explode(DIRECTORY_SEPARATOR, $input->getArgument("site-path"));
+        $filterEmptyPaths = array_filter($sitePathesToArray);
+        $sanitizedString = implode(DIRECTORY_SEPARATOR, $filterEmptyPaths);
+        return $input->setArgument('site-path', $sanitizedString);
     }
 }
